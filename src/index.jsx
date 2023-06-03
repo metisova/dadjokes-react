a87ae1b4010684db2c2c513ed948e225788704a5
Anna Kopylova <anna.metisova@gmail.com>
2023-06-03 14:11:41 +0200

    added main content of the app

--- src/index.jsx
+++ src/index.jsx
@@ -4,25 +4,36 @@ import './style.css';
 
 const App = () => {
   return (
-    <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
+    <div class="container">
+      <div class="joke">
+        <div class="joke__body">
+          <div class="joke__user">
+            <img
+              class="user-avatar"
+              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+            />
+            <p class="user-name">Neroxx</p>
+          </div>
+
+          <p class="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div class="joke__likes">
+          <button id="btn-up" class="btn-like btn-like--up"></button>
+          <span id="likes-up" class="likes-count likes-count--up">
+            0
+          </span>
+          <button id="btn-down" class="btn-like btn-like--down"></button>
+          <span id="likes-down" class="likes-count likes-count--down">
+            0
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
 
-createRoot(
-  document.querySelector('#app'),
-).render(<App />);
+createRoot(document.querySelector('#app')).render(<App />);
