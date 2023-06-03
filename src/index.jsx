6301e337864d8ad2932b63559c9682e58835b667
Anna Kopylova <anna.metisova@gmail.com>
2023-06-03 14:32:37 +0200

    changed class to className

--- src/index.jsx
+++ src/index.jsx
@@ -2,32 +2,33 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 
+
 const App = () => {
   return (
-    <div class="container">
-      <div class="joke">
-        <div class="joke__body">
-          <div class="joke__user">
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
             <img
-              class="user-avatar"
+              className="user-avatar"
               src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
             />
-            <p class="user-name">Neroxx</p>
+            <p className="user-name">Neroxx</p>
           </div>
 
-          <p class="joke__text">
+          <p className="joke__text">
             The secret service isn't allowed to yell "Get down!" anymore when
             the president is about to be attacked. Now they have to yell
             "Donald, duck!"
           </p>
         </div>
-        <div class="joke__likes">
-          <button id="btn-up" class="btn-like btn-like--up"></button>
-          <span id="likes-up" class="likes-count likes-count--up">
+        <div className="joke__likes">
+          <button id="btn-up" className="btn-like btn-like--up"></button>
+          <span id="likes-up" className="likes-count likes-count--up">
             0
           </span>
-          <button id="btn-down" class="btn-like btn-like--down"></button>
-          <span id="likes-down" class="likes-count likes-count--down">
+          <button id="btn-down" className="btn-like btn-like--down"></button>
+          <span id="likes-down" className="likes-count likes-count--down">
             0
           </span>
         </div>
