8f1ba223447c21b7d95a32a49c31b7d62357844c
Anna Kopylova <anna.metisova@gmail.com>
2023-06-03 15:42:51 +0200

    created a separete component Joke with it's own style.css and img

--- src/index.jsx
+++ src/index.jsx
@@ -1,56 +1,18 @@
 import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import Joke from './Joke';
 
 const App = () => {
-  const [like, setLike] = useState(0);
-  const [dislike, setDislike] = useState(0);
-
-  const handleLike = () => {
-    setLike(like + 1);
-  };
-
-  const handleDislike = () => {
-    setDislike(dislike + 1);
-  };
-
   return (
     <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button
-            id="btn-up"
-            className="btn-like btn-like--up"
-            onClick={handleLike}
-          ></button>
-          <span id="likes-up" className="likes-count likes-count--up">
-            {like}
-          </span>
-          <button
-            id="btn-down"
-            className="btn-like btn-like--down"
-            onClick={handleDislike}
-          ></button>
-          <span id="likes-down" className="likes-count likes-count--down">
-            {dislike}
-          </span>
-        </div>
-      </div>
+      <Joke
+        userAvatar="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+        userName="Neroxx"
+        text="The secret service isn't allowed to yell 'Get down!' anymore when the president is about to be attacked. Now they have to yell 'Donald, duck!'"
+        likes={0}
+        dislikes={0}
+      />
     </div>
   );
 };
