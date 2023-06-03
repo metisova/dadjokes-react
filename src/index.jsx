5a4deb832751b0d727e0de4aac39338f956a300f
Anna Kopylova <anna.metisova@gmail.com>
2023-06-03 15:55:43 +0200

    imported all jokes from jokes.js

--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,21 @@ import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.css';
 import Joke from './Joke';
+import { jokes } from './jokes';
 
 const App = () => {
   return (
     <div className="container">
-      <Joke
-        userAvatar="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-        userName="Neroxx"
-        text="The secret service isn't allowed to yell 'Get down!' anymore when the president is about to be attacked. Now they have to yell 'Donald, duck!'"
-        likes={0}
-        dislikes={0}
-      />
+      {jokes.map((joke) => (
+        <Joke
+          key={joke.id}
+          userAvatar={joke.avatar}
+          userName={joke.name}
+          text={joke.text}
+          likes={joke.likes}
+          dislikes={joke.dislikes}
+        />
+      ))}
     </div>
   );
 };
